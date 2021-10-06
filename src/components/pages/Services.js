e3387461f4952619ae9b184414f6c3f71e11ddcb
Fungai Mawoyo <fungaimawoyo@gmail.com>
2021-10-06 16:03:07 -0600

    Finished adding styling to portfolio section. Just need to fix the smooth scrolling which I think might be an issue with the image sizes

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -78,234 +78,286 @@ export default function Services() {
           <img className='img-settings' src='images/OLI_0018.jpg'
             alt='Photography-Icon'
           />
-          Central Memorial Park
-          July, 2021
-          Calgary, AB, Canada
+          <p>
+            Central Memorial Park <br />
+            July, 2021 <br />
+            Calgary, AB, Canada <br />
+          </p>
         </div>
 
         <div className='image-container'>
           <img className='img-settings' src='images/DSC_0025.JPG'
             alt='Photography-Icon'
           />
-          Centre Street Train Platform
-          March, 2020
-          Calgary, AB, Canada
+          <p>
+            Centre Street Train Platform <br />
+            March, 2020 <br />
+            Calgary, AB, Canada
+          </p>
         </div>
 
         <div className='image-container'>
           <img className='img-settings' src='images/OLI_0029.JPG'
             alt='Photography-Icon'
           />
-          Sunalta Train Platform
-          July, 2020
-          Calgary, AB, Canada
+          <p>
+            Sunalta Train Platform <br />
+            July, 2020 <br />
+            Calgary, AB, Canada
+          </p>
         </div>
 
         <div className='image-container'>
           <img className='img-settings' src='images/OLI_0042.jpg'
             alt='Photography-Icon'
           />
-          Sunalta
-          April, 2021
-          Calgary, AB, Canada
+          <p>
+            Sunalta <br />
+            April, 2021 <br />
+            Calgary, AB, Canada
+          </p>
         </div>
 
         <div className='image-container'>
           <img className='img-settings' src='images/OLI_0087.jpg'
             alt='Photography-Icon'
           />
-          Chinatown
-          April, 2021
-          Calgary, AB, Canada
+          <p>
+            Chinatown <br />
+            April, 2021 <br />
+            Calgary, AB, Canada
+          </p>
         </div>
 
         <div className='image-container'>
           <img className='img-settings' src='images/OLI_0100.jpg'
             alt='Photography-Icon'
           />
-          Banff Tea Co
-          February, 2021
-          Banff, AB, Canada
+          <p>
+            Banff Tea Co <br />
+            February, 2021 <br />
+            Banff, AB, Canada
+          </p>
         </div>
 
         <div className='image-container'>
           <img className='img-settings' src='images/OLI_0101.jpg'
             alt='Photography-Icon'
           />
-          Calgary Tennis CLub
-          August, 2020
-          Calgary, AB, Canada
+          <p>
+            Calgary Tennis Club <br />
+            August, 2020 <br />
+            Calgary, AB, Canada
+          </p>
         </div>
 
         <div className='image-container'>
           <img className='img-settings' src='images/OLI_0246.jpg'
             alt='Photography-Icon'
           />
-          3rd Street SW Train Station
-          November, 2020
-          Calgary, AB, Canada
+          <p>
+            3rd Street SW Train Station <br />
+            November, 2020 <br />
+            Calgary, AB, Canada
+          </p>
         </div>
 
         <div className='image-container'>
           <img className='img-settings' src='images/OLI_0001.jpg'
             alt='Photography-Icon'
           />
-          Bankview
-          September, 2020
-          Calgary, AB, Canada
+          <p>
+            Bankview <br />
+            September, 2020 <br />
+            Calgary, AB, Canada
+          </p>
         </div>
 
         <div className='image-container'>
           <img className='img-settings' src='images/OLI_0006.jpg'
             alt='Photography-Icon'
           />
-          Bankview
-          December, 2020
-          Calgary, AB, Canada
+          <p>
+            Bankview <br />
+            December, 2020 <br />
+            Calgary, AB, Canada
+          </p>
         </div>
 
         <div className='image-container'>
           <img className='img-settings' src='images/OLI_0007.jpg'
             alt='Photography-Icon'
           />
-          Bankview
-          September, 2020
-          Calgary, AB, Canada
+          <p>
+            Bankview <br />
+            September, 2020 <br />
+            Calgary, AB, Canada
+          </p>
         </div>
 
         <div className='image-container'>
           <img className='img-settings' src='images/OLI_0014.jpg'
             alt='Photography-Icon'
           />
-          Bankview
-          July, 2020
-          Calgary, AB, Canada
+          <p>
+            Bankview <br />
+            July, 2020 <br />
+            Calgary, AB, Canada
+          </p>
         </div>
 
         <div className='image-container'>
           <img className='img-settings' src='images/OLI_0016.jpg'
             alt='Photography-Icon'
           />
-          17th Ave SW
-          December, 2020
-          Calgary, AB, Canada
+          <p>
+            17th Ave SW <br />
+            December, 2020 <br />
+            Calgary, AB, Canada
+          </p>
         </div>
 
         <div className='image-container'>
           <img className='img-settings' src='images/OLI_0033.jpg'
             alt='Photography-Icon'
           />
-          Lindsay Park
-          September, 2021
-          Calgary, AB, Canada
+          <p>
+            Lindsay Park <br />
+            September, 2021 <br />
+            Calgary, AB, Canada
+          </p>
         </div>
 
         <div className='image-container'>
           <img className='img-settings' src='images/OLI_0022(1).jpg'
             alt='Photography-Icon'
           />
-          Downtown
-          June, 2020
-          Calgary, AB, Canada
+          <p>
+            Downtown <br />
+            June, 2020 <br />
+            Calgary, AB, Canada
+          </p>
         </div>
 
         <div className='image-container'>
           <img className='img-settings' src='images/OLI_0034.jpg'
             alt='Photography-Icon'
           />
-          Sunalta Train Platform
-          July, 2020
-          Calgary, AB, Canada
+          <p>
+            Sunalta Train Platform <br />
+            July, 2020 <br />
+            Calgary, AB, Canada
+          </p>
         </div>
 
         <div className='image-container'>
           <img className='img-settings' src='images/OLI_0040.jpg'
             alt='Photography-Icon'
           />
-          Central Memorial Park
-          September, 2021
-          Calgary, AB, Canada
+          <p>
+            Central Memorial Park <br />
+            September, 2021 <br />
+            Calgary, AB, Canada
+          </p>
         </div>
 
         <div className='image-container'>
           <img className='img-settings' src='images/OLI_0074.jpg'
             alt='Photography-Icon'
           />
-          Chinatown
-          April, 2021
-          Calgary, AB, Canada
+          <p>
+            Chinatown <br />
+            April, 2021 <br />
+            Calgary, AB, Canada
+          </p>
         </div>
 
         <div className='image-container'>
           <img className='img-settings' src='images/OLI_0085.jpg'
             alt='Photography-Icon'
           />
-          Bankview
-          February, 2021
-          Calgary, AB, Canada
+          <p>
+            Bankview <br />
+            February, 2021 <br />
+            Calgary, AB, Canada
+          </p>
         </div>
 
         <div className='image-container'>
           <img className='img-settings' src='images/OLI_0094.jpg'
             alt='Photography-Icon'
           />
-          Bankview
-          September, 2020
-          Calgary, AB, Canada
+          <p>
+            Bankview <br />
+            September, 2020 <br />
+            Calgary, AB, Canada
+          </p>
         </div>
 
         <div className='image-container'>
           <img className='img-settings' src='images/OLI_0105.jpg'
             alt='Photography-Icon'
           />
-          Bankview
-          July, 2020
-          Calgary, AB, Canada
+          <p>
+            Bankview <br />
+            July, 2020 <br />
+            Calgary, AB, Canada
+          </p>
         </div>
 
         <div className='image-container'>
           <img className='img-settings' src='images/OLI_0148.jpg'
             alt='Photography-Icon'
           />
-          Downtown
-          September, 2020
-          Calgary, AB, Canada
+          <p>
+            Downtown <br />
+            September, 2020 <br />
+            Calgary, AB, Canada
+          </p>
         </div>
 
         <div className='image-container'>
           <img className='img-settings' src='images/OLI_0151.jpg'
             alt='Photography-Icon'
           />
-          Sunalta
-          August, 2020
-          Calgary, AB, Canada
+          <p>
+            Sunalta <br />
+            August, 2020 <br />
+            Calgary, AB, Canada
+          </p>
         </div>
 
         <div className='image-container'>
           <img className='img-settings' src='images/OLI_0190.jpg'
             alt='Photography-Icon'
           />
-          17th Ave SW
-          November, 2020
-          Calgary, AB, Canada
+          <p>
+            17th Ave SW <br />
+            November, 2020 <br />
+            Calgary, AB, Canada
+          </p>
         </div>
 
         <div className='image-container'>
           <img className='img-settings' src='images/OLI_0198.jpg'
             alt='Photography-Icon'
           />
-          Downtown West End
-          November, 2020
-          Calgary, AB, Canada
+          <p>
+            Downtown West End <br />
+            November, 2020 <br />
+            Calgary, AB, Canada
+          </p>
         </div>
 
         <div className='image-container'>
           <img className='img-settings' src='images/OLI_0211.jpg'
             alt='Photography-Icon'
           />
-          Downtown
-          November, 2020
-          Calgary, AB, Canada
+          <p>
+            Downtown <br />
+            November, 2020 <br />
+            Calgary, AB, Canada
+          </p>
         </div>
       </div>
     </motion.div>
