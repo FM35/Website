3c87a86bf307bdc855242606a6efd624c1843e03
Fungai Mawoyo <fungaimawoyo@gmail.com>
2021-10-26 17:55:56 -0600

    Added extra styling to Contact Me. I'll work on PC Builds and get back to the contact me section

--- src/components/pages/Sign_up.js
+++ src/components/pages/Sign_up.js
@@ -5,17 +5,20 @@ export default function Services() {
 
   return (
     <div className='about-me-container'>
-
-      <h1>Contact Me</h1>
-      <p>Contact me for any business enquries that align with Website/FrontEnd developing, Computer Builds, Virtual Exhibit or photography</p>
-      <form className='form' action="https://formsubmit.co/fungaimawoyo@gmail.com" method="POST">
-        <input type="hidden" name="_captcha" value="false" />
-        <input style={{ width: '400px' }} type="text" name="full-name" placeholder="Full Name" required />
-        <input style={{ width: '400px' }} type="text" name="subject" placeholder="Subject" required />
-        <input style={{ width: '400px' }} type="email" name="email" placeholder="Email Address" required />
-        <textarea style={{ width: '400px', height: '319px' }} placeholder="Type Your Message Here" required></textarea>
-        <button style={{ width: '100px' }} type="submit">Send</button>
-      </form>
+      <div className='contact-me'>
+        <h1 className='position-1'>Contact Me</h1>
+        <p className='position-1'>Contact me for any business enquries that align with Website/FrontEnd developing, Computer Builds, Virtual Exhibit or photography</p>
+        <div className='position-1'>
+          <form className='form-inline' action="https://formsubmit.co/fungaimawoyo@gmail.com" method="POST">
+            <input type="hidden" name="_captcha" value="false" />
+            <input type="text" name="full-name" placeholder="Full Name" required />
+            <input type="text" name="subject" placeholder="Subject" required />
+            <input type="email" name="email" placeholder="Email Address" required />
+            <textarea placeholder="Type Your Message Here" required></textarea><br />
+            <button type="submit">Send</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
